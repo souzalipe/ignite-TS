80544d5a04dfd7c25d8e3f28c782fcd66ccde9eb
Felipe Nascimento de Souza <fn23886@gmail.com>
2024-12-23 16:15:09 -0300

    feat: atualizar conteúdo dos posts e adicionar novo post sobre aula de TypeScript

--- ignite/src/App.tsx
+++ ignite/src/App.tsx
@@ -5,26 +5,28 @@ import SideBar from "./components/SideBar/SideBar.tsx";
 import AppStylesModule from "./styles/App.module.css";
 import "./styles/global.css";
 
-// author: { avatar_url: "", name: "", role: ""}
-// publishedAt: Date
-// content: string
 
 const posts: PostType[] = [
   {
     id: 1,
     author: {
-      avatarUrl: "https://github.com/souzalipe.png",
-      name: "Felipe Souza",
-      role: "Programadores do Amanhã",
+      avatarUrl: "https://github.com/Nathaliaaguiar.png",
+      name: "Nathalia Aguiar",
+      role: "Desenvolvedora Front-end",
     },
     content: [
-      { type: "pharagraph", content: "Fala galeraa 👋" },
       {
         type: "pharagraph",
         content:
-          "Acabei de subir mais um projeto no meu portifólio. É um projeto que fiz",
+          "✨ Concluí a trilha de React Native no NLW Pocket: Mobile da Rocketseat! ✨",
       },
-      { type: "link", content: "https://github.com/souzalipe" },
+
+      {
+        type: "pharagraph",
+        content:
+          "Entre os dias 09 e 13 de dezembro de 2024, tive o privilégio de participar deste evento incrível, guiado pelo educador Rodrigo Gonçalves , que tornou o aprendizado leve, dinâmico e muito prático. Foi o meu primeiro contato com React Native.",
+      },
+      { type: "link", content: "https://github.com/Nathaliaaguiar" },
     ],
     publishedAt: new Date("2024-12-03 20:00"),
   },
@@ -45,6 +47,29 @@ const posts: PostType[] = [
       { type: "link", content: "https://github.com/brenddamachado" },
     ],
     publishedAt: new Date("2024-12-06 20:00"),
+  },
+  {
+    id: 3,
+    author: {
+      avatarUrl: "https://github.com/souzalipe.png",
+      name: "Felipe Souza",
+      role: "Programadores do Amanhã",
+    },
+    content: [
+      {
+        type: "pharagraph",
+        content:
+          "🎉 Dei aula para uma Sênior e olha no que deu! 😨",
+      },
+
+      {
+        type: "pharagraph",
+        content:
+        "Na segunda-feira, durante a Mão no Código na Programadores do Amanhã, discutimos dois conceitos fundamentais do TypeScript: União (|) e Interseção (&). 🛠️ Antes disso, a professora Iasmim Oliveira já tinha me proposto um grande desafio: dar essa aula e, de quebra, encontrar uma ligação matemática entre esses conceitos. E o mais interessante? Eu só sabia o básico de TypeScript! Mesmo assim, o resultado foi incrível:"
+      },
+      { type: "link", content:"link do post no LinkedIn" },
+    ],
+    publishedAt: new Date("2024-12-23 10:00"),
   },
 ];
 
